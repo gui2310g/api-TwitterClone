6b10c2d193ecd750a6d2e7d9a6b66ea3f708069b
gui2310g <fontesguilherme57@gmail.com>
2024-11-09 23:30:31 -0300

    created userservice and usercontroller

--- src/main/java/com/example/api_TwitterClone/repositories/UsersRepository.java
+++ src/main/java/com/example/api_TwitterClone/repositories/UsersRepository.java
@@ -2,10 +2,12 @@ package com.example.api_TwitterClone.repositories;
 
 import com.example.api_TwitterClone.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, String> {
     Optional<Users> findByEmail(String email);
 
---
+++ src/main/java/com/example/api_TwitterClone/services/UsersService.java
@@ -0,0 +1,11 @@
+package com.example.api_TwitterClone.services;
+
+import com.example.api_TwitterClone.repositories.UsersRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    private UsersRepository usersRepository;
+
+}
