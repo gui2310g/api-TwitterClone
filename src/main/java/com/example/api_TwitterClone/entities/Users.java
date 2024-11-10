542f0f7704b4a579af5208b2f7b1d891a82b4439
gui2310g <fontesguilherme57@gmail.com>
2024-11-09 23:19:00 -0300

    created users entity

---
+++ src/main/java/com/example/api_TwitterClone/entities/Users.java
@@ -0,0 +1,34 @@
+package com.example.api_TwitterClone.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "avatar", nullable = false)
+    private String avatar;
+
+    @Column(name = "background", nullable = false)
+    private String background;
+
+    public Users() {}
+}
