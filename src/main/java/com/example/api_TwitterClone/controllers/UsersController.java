af97d9bed33084cdb367e3c134a66bf74e0c1346
gui2310g <fontesguilherme57@gmail.com>
2024-11-10 00:31:11 -0300

    added createUser in userscontroller

--- src/main/java/com/example/api_TwitterClone/controllers/UsersController.java
+++ src/main/java/com/example/api_TwitterClone/controllers/UsersController.java
@@ -1,7 +1,13 @@
 package com.example.api_TwitterClone.controllers;
 
+import com.example.api_TwitterClone.entities.Users;
 import com.example.api_TwitterClone.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,11 @@ public class UsersController {
 
     @Autowired
     private UsersService usersService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Users> createUser(@RequestBody Users users) {
+        Users createdUser = usersService.createUser(users);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
 }
+
