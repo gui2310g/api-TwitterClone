df8eb15fc8ceb5cf38cdec546b36e763abfb8611
gui2310g <fontesguilherme57@gmail.com>
2024-11-16 01:01:09 -0300

    created userRequest dto

---
+++ src/main/java/com/example/api_TwitterClone/dto/UserRequest.java
@@ -0,0 +1,13 @@
+package com.example.api_TwitterClone.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+    private final String username;
+    private final String email;
+    private final String password;
+    private final String name;
+    private final String avatar;
+    private final String background;
+}
